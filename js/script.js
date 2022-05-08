09d344e75c9db66757a4ef7827c9851f7a529c10
Bondi14 <Bondi14>
2022-05-08 16:05:01 +0300

    added fetch and promises

--- js/script.js
+++ js/script.js
@@ -219,11 +219,8 @@ class MenuCard {
             `;
             form.insertAdjacentElement('afterend', statusMessage)
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-
             //request.setRequestHeader('Content-type', 'multipart/form-data');
-            request.setRequestHeader('Content-type', 'application/json');
+            //request.setRequestHeader();
             const formData = new FormData(form);
 
             const object = {};
@@ -231,19 +228,22 @@ class MenuCard {
                 object[key] = value;
             }); 
 
-            const json = JSON.stringify(object);
-
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200){
-                    console.log(request.response);
+            fetch('server.php', {
+                method: "POST",
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            }).then(data => data.text())
+            .then(data => {
+                    console.log(data);
                     showThanksModal(message.success);
-                    form.reset();
-                } else {
-                    showThanksModal(message.failure);
-                }
-            });
+                    statusMessage.remove();  
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
+            })
         });
     }
 
